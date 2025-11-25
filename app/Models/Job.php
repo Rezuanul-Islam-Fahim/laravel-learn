bce94e472260f5d8290635ca3dca1b4e32001ac3
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-11-25 15:37:52 +0600

    Add Tag model, factory, and migration; establish relationships with Job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,18 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            \App\Models\Tag::class,
+            table: 'job_tags',
+            foreignPivotKey: 'job_listing_id'
+        );
+    }
 }
